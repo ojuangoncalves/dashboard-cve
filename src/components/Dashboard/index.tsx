4537bc991b79e83ae8623ea6ffb5ec06d540addb
Rafaaelsz <rafaeltperaldo@gmail.com>
2025-08-20 15:13:14 -0300

    Revert "- criado o componente dos cards"
    This reverts commit 7dc72f22e5be5d7fb0dcda52a06123df3b334c06.

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,106 +1,89 @@
-"use client";
+'use client'
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-import { getChargePointsData } from "@/utils/util";
-import Caption from "@/components/Caption";
-import Notifications from "@/components/Notifications";
-import homeCard from "./../Cards/index";
-import HomeCard from "./../Cards/index";
+import { getChargePointsData } from '@/utils/util'
+import Caption from '@/components/Caption'
+import Notifications from '@/components/Notifications'
 
 interface DashboardProps {
-  headers: customRequestHeaders;
+	headers: customRequestHeaders
 }
 
+
 export default function Dashboard(props: DashboardProps) {
-  const [chargePoints, setChargePoints] = useState<ChargePoint[]>([]);
-  // ciclo de requisições na API
-  useEffect(() => {
-    getChargePointsData(props.headers, setChargePoints);
 
-    const interval = setInterval(() => {
-      getChargePointsData(props.headers, setChargePoints);
-    }, 60000);
+	const [chargePoints, setChargePoints] = useState<ChargePoint[]>([])
+	// ciclo de requisições na API
+	useEffect(() => {
+		getChargePointsData(props.headers, setChargePoints)
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+		const interval = setInterval(() => {
+			getChargePointsData(props.headers, setChargePoints)
+		}, 60000)
 
-  return (
-    <main className="flex flex-col justify-between gap-14">
-      <header className="w-full h-12 flex justify-center items-center">
-        <h1 className="text-center text-3xl md:text-5xl font-bold">
-          Monitoramento CVE - Balneário Shopping
-        </h1>
-      </header>
+		return () => {
+			clearInterval(interval)
+		}
+	}, [])
 
-      <section className="flex flex-col-reverse items-center gap-10 lg:grid xl:grid-cols-3 lg:grid-cols-2 lg:items-start">
-        {/* <Notifications headers={props.headers} /> */}
-        <HomeCard />
+    return (
+        <main className='flex flex-col justify-between gap-14'>
+		<header className='w-full h-12 flex justify-center items-center'>
+			<h1 className='text-center text-3xl md:text-5xl font-bold'>Monitoramento CVE - Balneário Shopping</h1>
+		</header>
 
-        <div className="flex flex-col justify-between gap-14 xl:col-span-2">
-          <Caption />
+		<section className='flex flex-col-reverse items-center gap-10 lg:grid xl:grid-cols-3 lg:grid-cols-2 lg:items-start'>
+			
+			<Notifications headers={props.headers} />
 
-          <div className="grid-cols-2 md:grid-cols-3 lg:grid-cols-2 grid xl:grid-cols-3 w-full m-auto place-items-center gap-y-10 gap-x-5">
-            {chargePoints.map((chargepoint) =>
-              chargepoint.connectors.map((connector) => {
-                switch (connector.lastStatus.status) {
-                  case "Available":
-                  case "SuspendedEV":
-                    return (
-                      <div
-                        key={chargepoint.chargeBoxPk}
-                        className="bg-[#2e8024] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl"
-                      >
-                        <img src={`/cveverde.png`} alt="" />
-                        <p className="text-sm md:text-base">
-                          {chargepoint.description}
-                        </p>
-                      </div>
-                    );
-                  case "Unavailable":
-                  case "Faulted":
-                    return (
-                      <div
-                        key={chargepoint.chargeBoxPk}
-                        className="bg-[#ed1c00] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl animate-pulse-alert"
-                      >
-                        <img src={`/cvevermelho.png`} alt="" />
-                        <p className="text-sm md:text-base">
-                          {chargepoint.description}
-                        </p>
-                      </div>
-                    );
-                  case "Charging":
-                  case "Finishing":
-                  case "Preparing":
-                  case "SuspendedEVSE":
-                    return (
-                      <div
-                        key={chargepoint.chargeBoxPk}
-                        className="bg-[#e4c306] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl"
-                      >
-                        <img src={`/cveamarelo.png`} alt="" />
-                        <p className="">{chargepoint.description}</p>
-                      </div>
-                    );
-                  case "Maintenance":
-                    return (
-                      <div
-                        key={chargepoint.chargeBoxPk}
-                        className="bg-[#525252] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl"
-                      >
-                        <img src={`/cvecinza.png`} alt="" />
-                        <p className="">{chargepoint.description}</p>
-                      </div>
-                    );
-                }
-              })
-            )}
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+			<div className='flex flex-col justify-between gap-14 xl:col-span-2'>
+				<Caption />
+
+
+				<div className='grid-cols-2 md:grid-cols-3 lg:grid-cols-2 grid xl:grid-cols-3 w-full m-auto place-items-center gap-y-10 gap-x-5'>
+					{  
+						chargePoints.map(chargepoint => chargepoint.connectors.map(connector => {
+								switch (connector.lastStatus.status) {
+									case "Available":
+									case "SuspendedEV":
+										return (
+											<div key={chargepoint.chargeBoxPk} className='bg-[#2e8024] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl'>
+											<img src={`/cveverde.png`} alt="" />
+											<p className="text-sm md:text-base">{ chargepoint.description}</p>
+										</div>
+										)
+									case "Unavailable":
+									case "Faulted":
+										return (
+											<div key={chargepoint.chargeBoxPk} className='bg-[#ed1c00] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl animate-pulse-alert'>
+												<img src={`/cvevermelho.png`} alt="" />
+												<p className="text-sm md:text-base">{ chargepoint.description}</p>
+											</div>
+										)
+									case "Charging":
+									case "Finishing":
+									case "Preparing":
+									case "SuspendedEVSE":
+										return (
+											<div key={chargepoint.chargeBoxPk} className='bg-[#e4c306] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl'>
+												<img src={`/cveamarelo.png`} alt="" />
+												<p className="">{ chargepoint.description}</p>
+											</div>
+										)
+									case "Maintenance":
+										return (
+											<div key={chargepoint.chargeBoxPk} className='bg-[#525252] w-28 h-28 md:w-40 md:h-40 flex flex-col justify-center items-center text-center rounded-xl'>
+												<img src={`/cvecinza.png`} alt="" />
+												<p className="">{ chargepoint.description}</p>
+											</div>
+										)
+										}
+							}))
+					}
+				</div>
+			</div>
+		</section>
+	</main>
+    )
 }
